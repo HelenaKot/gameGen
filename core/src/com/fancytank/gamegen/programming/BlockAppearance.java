062b78d17faec0cbbcadb4fd314beaaf14eb9a2d
rilane <s10474@pjwstk.edu.pl>
2016-05-02 20:08:02 +0200

    refactoring

--- core/src/com/fancytank/gamegen/programming/BlockAppearance.java
+++ core/src/com/fancytank/gamegen/programming/BlockAppearance.java
@@ -6,16 +6,18 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
+import static com.fancytank.gamegen.programming.Direction.*;
+
 public class BlockAppearance {
     Label label;
-    PatchData[] patch = new PatchData[4]; // topPatch, leftPatch, rightPatch, bottomPatch;
+    PatchData[] patch = new PatchData[4];
     static Skin skin;
     static int padding = 16;
 
     BlockAppearance(Actor root, String labelText, BlockShape shape) {
         label = new Label(labelText, skin);
 
-        for (Direction dir : Direction.values())
+        for (Direction dir : values())
             patch[dir.ordinal()] = new PatchData(getPatchTexture(shape.connects(dir), dir));
 
         setSize(root);
@@ -27,27 +29,27 @@ public class BlockAppearance {
     }
 
     void setPosition(float x, float y) {
-        patch[Direction.DOWN.ordinal()].setPosition(x, y);
-        patch[Direction.LEFT.ordinal()].setPosition(x, y + padding);
-        patch[Direction.RIGHT.ordinal()].setPosition(x + padding, y + padding);
-        patch[Direction.UP.ordinal()].setPosition(x, y + label.getHeight() + padding);
+        patch[DOWN.ordinal()].setPosition(x, y);
+        patch[LEFT.ordinal()].setPosition(x, y + padding);
+        patch[RIGHT.ordinal()].setPosition(x + padding, y + padding);
+        patch[UP.ordinal()].setPosition(x, y + label.getHeight() + padding);
         label.setPosition(x + padding, y + padding);
     }
 
     void translate(float x, float y) {
-        setPosition(patch[Direction.DOWN.ordinal()].startX + x, patch[Direction.DOWN.ordinal()].startY + y);
+        setPosition(patch[DOWN.ordinal()].startX + x, patch[DOWN.ordinal()].startY + y);
     }
 
     private void setSize(Actor root) {
-        patch[Direction.UP.ordinal()].setSize(label.getWidth() + padding * 2, padding);
-        patch[Direction.LEFT.ordinal()].setSize(padding, label.getHeight());
-        patch[Direction.RIGHT.ordinal()].setSize(label.getWidth() + padding, label.getHeight());
-        patch[Direction.DOWN.ordinal()].setSize(label.getWidth() + padding * 2, padding);
+        patch[UP.ordinal()].setSize(label.getWidth() + padding * 2, padding);
+        patch[LEFT.ordinal()].setSize(padding, label.getHeight());
+        patch[RIGHT.ordinal()].setSize(label.getWidth() + padding, label.getHeight());
+        patch[DOWN.ordinal()].setSize(label.getWidth() + padding * 2, padding);
         root.setBounds(0, 0, label.getWidth() + padding * 2, label.getHeight() + padding * 2);
     }
 
     void drawShape(Batch batch, float alpha) {
-        for (Direction dir : Direction.values())
+        for (Direction dir : values())
             drawPatch(patch[dir.ordinal()], batch);
         label.draw(batch, alpha);
     }
