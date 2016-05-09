65b5f8128bb6a8f5313ce864c975166346b07ae4
rilane <s10474@pjwstk.edu.pl>
2016-05-09 22:20:33 +0200

    deleted unnecessary vector in ConnectionPlacer

--- core/src/com/fancytank/gamegen/programming/looks/ConnectionPlacer.java
+++ core/src/com/fancytank/gamegen/programming/looks/ConnectionPlacer.java
@@ -1,6 +1,5 @@
 package com.fancytank.gamegen.programming.looks;
 
-import com.badlogic.gdx.math.Vector2;
 import com.fancytank.gamegen.programming.Direction;
 
 import java.util.ArrayList;
@@ -14,20 +13,19 @@ public class ConnectionPlacer {
         coreBlock.getWidth();
         for (Direction direction : Direction.values())
             if (coreBlock.data.shape.connects(direction)) {
-                Vector2 pos = coreBlock.localToStageCoordinates(new Vector2(0, 0));
                 float width = coreBlock.getWidth(), height = coreBlock.getHeight();
                 switch (direction) {
                     case UP:
-                        output.add(new ConnectionArea(pos.x, pos.y + height - padding, coreBlock, Direction.UP));
+                        output.add(new ConnectionArea(0, height - padding, coreBlock, Direction.UP));
                         break;
                     case RIGHT:
-                        output.add(new ConnectionArea(pos.x + width - padding, pos.y + padding, coreBlock, Direction.RIGHT));
+                        output.add(new ConnectionArea(0 + width - padding, padding, coreBlock, Direction.RIGHT));
                         break;
                     case DOWN:
-                        output.add(new ConnectionArea(pos.x, pos.y, coreBlock, Direction.DOWN));
+                        output.add(new ConnectionArea(0, 0, coreBlock, Direction.DOWN));
                         break;
                     case LEFT:
-                        output.add(new ConnectionArea(pos.x, pos.y + padding, coreBlock, Direction.LEFT));
+                        output.add(new ConnectionArea(0, height - padding * 2, coreBlock, Direction.LEFT));
                         break;
                 }
             }
