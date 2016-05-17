bf56d9f0cabe15e392272cb96ac53dc956bab9e8
rilane <s10474@pjwstk.edu.pl>
2016-05-17 19:18:21 +0200

    xml cleanup + refactoring

--- android/src/com/fancytank/gamegen/AndroidLauncher.java
+++ android/src/com/fancytank/gamegen/AndroidLauncher.java
@@ -1,11 +1,7 @@
 package com.fancytank.gamegen;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,49 +9,32 @@ import android.widget.FrameLayout;
 import android.widget.ListView;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.wunderlist.slidinglayer.SlidingLayer;
 
 public class AndroidLauncher extends AndroidApplication {
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
+    private ListView listOfBlocks;
+    private SlidingLayer slidingLayer;
     private String[] mRandomList = {"derp", "herp"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-       // AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 
         FrameLayout contentFrame = (FrameLayout) findViewById(R.id.content_frame);
         View gdxView = initializeForView(new AndroidGameGenerator());
+
+        slidingLayer = (SlidingLayer) findViewById(R.id.sliding_layer);
+
         contentFrame.addView(gdxView);
-
-        //setUpDrawer();
+        setUpListOfBlocks();
     }
 
-    private void setUpSlidingLayer() {
-        SlidingLayer slidingLayer = (SlidingLayer) findViewById(R.id.slidingLayer1);
-
-
-        slidingLayer.setChangeStateOnTap(false);
-    }
-
-//    private void setUpDrawer() {
-//       // mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-//        mDrawerList = (ListView) findViewById(R.id.drawer_list);
-//
-//        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-//                R.layout.drawer_list_item, mRandomList));
-//        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-//    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        Log.e("LOG", "selected option " + item.getItemId());
-        return true;
+    private void setUpListOfBlocks() {
+        listOfBlocks = (ListView) findViewById(R.id.drawer_list);
+        listOfBlocks.setAdapter(new ArrayAdapter<>(this,
+                R.layout.drawer_list_item, mRandomList));
+        listOfBlocks.setOnItemClickListener(new DrawerItemClickListener());
     }
 
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
@@ -66,29 +45,9 @@ public class AndroidLauncher extends AndroidApplication {
     }
 
     private void selectItem(int position) {
-        Log.e("LOG", "selected item!");
-        mDrawerList.setItemChecked(position, true);
-        mDrawerLayout.closeDrawer(mDrawerList);
+        Log.e("LOG", "selected item- " + position);
+        listOfBlocks.setItemChecked(position, true);
+        slidingLayer.closeLayer(true);
     }
-
-    /**
-     * When using the ActionBarDrawerToggle, you must call it during
-     * onPostCreate() and onConfigurationChanged()...
-     */
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        //  mDrawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        // Pass any configuration change to the drawer toggls
-        //   mDrawerToggle.onConfigurationChanged(newConfig);
-    }
-
 
 }
