eb0c967ba06affd49e1c4007fe4f1951c6089bbf
rilane <s10474@pjwstk.edu.pl>
2016-05-17 22:02:38 +0200

    expenableListView demo pasted inside project

--- android/src/com/fancytank/gamegen/AndroidLauncher.java
+++ android/src/com/fancytank/gamegen/AndroidLauncher.java
@@ -1,20 +1,23 @@
 package com.fancytank.gamegen;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
 import android.widget.FrameLayout;
 import android.widget.ListView;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.wunderlist.slidinglayer.SlidingLayer;
 
+import java.util.ArrayList;
+
 public class AndroidLauncher extends AndroidApplication {
-    private ListView listOfBlocks;
     private SlidingLayer slidingLayer;
-    private String[] mRandomList = {"derp", "herp"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,31 +26,63 @@ public class AndroidLauncher extends AndroidApplication {
 
         FrameLayout contentFrame = (FrameLayout) findViewById(R.id.content_frame);
         View gdxView = initializeForView(new AndroidGameGenerator());
+        contentFrame.addView(gdxView);
 
         slidingLayer = (SlidingLayer) findViewById(R.id.sliding_layer);
+        //slidingLayer.closeLayer(true);
 
-        contentFrame.addView(gdxView);
-        setUpListOfBlocks();
+        setGroupParents();
+        setChildData();
+
+        ExpandableListView expandableList = (ExpandableListView) findViewById(R.id.drawer_list);
+        BlockExpendableListAdapter adapter = new BlockExpendableListAdapter(parentItems, childItems);
+
+        adapter.setInflater((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE), this);
+        expandableList.setAdapter(adapter);
     }
 
-    private void setUpListOfBlocks() {
-        listOfBlocks = (ListView) findViewById(R.id.drawer_list);
-        listOfBlocks.setAdapter(new ArrayAdapter<>(this,
-                R.layout.drawer_list_item, mRandomList));
-        listOfBlocks.setOnItemClickListener(new DrawerItemClickListener());
+    private ArrayList<String> parentItems = new ArrayList<String>();
+    private ArrayList<Object> childItems = new ArrayList<Object>();
+
+    public void setGroupParents() {
+        parentItems.add("Android");
+        parentItems.add("Core Java");
+        parentItems.add("Desktop Java");
+        parentItems.add("Enterprise Java");
     }
 
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectItem(position);
-        }
-    }
+    public void setChildData() {
 
-    private void selectItem(int position) {
-        Log.e("LOG", "selected item- " + position);
-        listOfBlocks.setItemChecked(position, true);
-        slidingLayer.closeLayer(true);
+        // Android
+        ArrayList<String> child = new ArrayList<String>();
+        child.add("Core");
+        child.add("Games");
+        childItems.add(child);
+
+        // Core Java
+        child = new ArrayList<String>();
+        child.add("Apache");
+        child.add("Applet");
+        child.add("AspectJ");
+        child.add("Beans");
+        child.add("Crypto");
+        childItems.add(child);
+
+        // Desktop Java
+        child = new ArrayList<String>();
+        child.add("Accessibility");
+        child.add("AWT");
+        child.add("ImageIO");
+        child.add("Print");
+        childItems.add(child);
+
+        // Enterprise Java
+        child = new ArrayList<String>();
+        child.add("EJB3");
+        child.add("GWT");
+        child.add("Hibernate");
+        child.add("JSP");
+        childItems.add(child);
     }
 
 }
---
+++ android/src/com/fancytank/gamegen/BlockExpendableListAdapter.java
@@ -0,0 +1,115 @@
+package com.fancytank.gamegen;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class BlockExpendableListAdapter extends BaseExpandableListAdapter {
+    private Activity activity;
+    private ArrayList<Object> childItems;
+    private LayoutInflater inflater;
+    private ArrayList<String> parentItems, child;
+
+    public BlockExpendableListAdapter(ArrayList<String> parents, ArrayList<Object> childern) {
+        this.parentItems = parents;
+        this.childItems = childern;
+    }
+
+    public void setInflater(LayoutInflater inflater, Activity activity) {
+        this.inflater = inflater;
+        this.activity = activity;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, final int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+
+        child = (ArrayList<String>) childItems.get(groupPosition);
+
+        TextView textView = null;
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.drawer_list_item, null);
+        }
+
+        textView = (TextView) convertView.findViewById(R.id.textView1);
+        textView.setText(child.get(childPosition));
+
+        convertView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(activity, child.get(childPosition),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return convertView;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.drawer_list_group, null);
+        }
+
+       ((TextView) convertView).setText(parentItems.get(groupPosition));
+
+        return convertView;
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return null;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return 0;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return ((ArrayList<String>) childItems.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return null;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return parentItems.size();
+    }
+
+    @Override
+    public void onGroupCollapsed(int groupPosition) {
+        super.onGroupCollapsed(groupPosition);
+    }
+
+    @Override
+    public void onGroupExpanded(int groupPosition) {
+        super.onGroupExpanded(groupPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+}
