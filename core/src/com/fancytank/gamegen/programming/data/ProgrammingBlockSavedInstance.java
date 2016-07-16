8d729c00ab499f0c2d03177b9dbc2caba042ec8a
rilane <s10474@pjwstk.edu.pl>
2016-07-16 05:46:58 +0200

    refactoring

--- core/src/com/fancytank/gamegen/programming/data/ProgrammingBlockSavedInstance.java
+++ core/src/com/fancytank/gamegen/programming/data/ProgrammingBlockSavedInstance.java
@@ -21,16 +21,12 @@ public class ProgrammingBlockSavedInstance implements Serializable {
         b = programmingBlock.coreBlock.tint.b;
         Vector2 pos = Utility.myLocalToStageCoordinates(programmingBlock);
         posX = pos.x + programmingBlock.getWidth();
-        System.out.println("pos" + pos.x + " " + programmingBlock.coreBlock.getWidth());
         posY = pos.y + programmingBlock.getHeight();
-        System.out.println("pos" + pos.y + " " + programmingBlock.coreBlock.getHeight());
     }
 
     public ProgrammingBlock restore() {
         ProgrammingBlock restoredBlock = new ProgrammingBlock(data, new Color(r, g, b, a));
         restoredBlock.setPosition(posX - restoredBlock.coreBlock.getWidth(), posY - restoredBlock.coreBlock.getHeight());
-        System.out.println("pos" +posX + " " + restoredBlock.coreBlock.getWidth());
-        System.out.println("pos" +posY + " " + restoredBlock.coreBlock.getHeight());
         return restoredBlock;
     }
 }
