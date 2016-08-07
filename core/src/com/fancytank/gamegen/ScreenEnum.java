9150359c6fe96e22fa896f69587faa9a3e83291f
rilane <s10474@pjwstk.edu.pl>
2016-08-07 12:27:42 +0200

    refactoring

--- core/src/com/fancytank/gamegen/ScreenEnum.java
+++ core/src/com/fancytank/gamegen/ScreenEnum.java
@@ -4,19 +4,19 @@ public enum ScreenEnum {
     EDITOR_SCREEN {
         @Override
         public AbstractScreen getScreen() {
-            return saveInstance(new EditorScreen());
+            return setInstance(new EditorScreen());
         }
     },
     GAME_SCREEN {
         @Override
         public AbstractScreen getScreen() {
-            return saveInstance(new GameScreen());
+            return setInstance(new GameScreen());
         }
     };
 
     static public AbstractScreen screenInstance;
 
-    private static AbstractScreen saveInstance(AbstractScreen screen) {
+    private static AbstractScreen setInstance(AbstractScreen screen) {
         if (screenInstance != null)
             screenInstance.dispose();
         screenInstance = screen;
