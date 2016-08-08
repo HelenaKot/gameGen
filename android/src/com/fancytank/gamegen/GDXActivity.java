777dc52b31942909c6a91d9d2d1a6917982498fb
rilane <s10474@pjwstk.edu.pl>
2016-08-08 02:35:11 +0200

    GDX cleanup, moved debug stuff

--- android/src/com/fancytank/gamegen/GDXActivity.java
+++ android/src/com/fancytank/gamegen/GDXActivity.java
@@ -1,19 +1,18 @@
 package com.fancytank.gamegen;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ExpandableListView;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.fancytank.gamegen.GameScreen;
-import com.fancytank.gamegen.MainGdx;
-import com.fancytank.gamegen.R;
-import com.fancytank.gamegen.ScreenEnum;
 import com.fancytank.gamegen.data.DataManager;
 import com.fancytank.gamegen.editor.BlockButton;
 import com.fancytank.gamegen.programming.BlocksExpendableList;
@@ -28,7 +27,7 @@ import org.greenrobot.eventbus.Subscribe;
 import java.io.IOException;
 
 public class GDXActivity extends AndroidApplication {
-    private FrameLayout contentFrame;
+    private FrameLayout gdxFrame;
     private SlidingLayer slidingLayer;
     private BlocksExpendableList list;
     private TextView debugText;
@@ -39,22 +38,25 @@ public class GDXActivity extends AndroidApplication {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_programming);
+        setContentView(R.layout.activity_gdx);
 
-        init();
+        initDebugTools();
+        initXML();
+        readName();
         EventBus.getDefault().register(this);
+    }
 
+    private void initXML() {
+        gdxFrame = (FrameLayout) findViewById(R.id.content_frame);
+        gdxFrame.addView(initializeForView(new MainGdx()));
+        slidingLayer = (SlidingLayer) findViewById(R.id.sliding_layer);
+        list = new BlocksExpendableList((ExpandableListView) findViewById(R.id.drawer_list), this);
+    }
+
+    private void readName() {
         Intent intent = getIntent();
         if (intent.hasExtra("saveName"))
             saveName = intent.getExtras().getString("saveName");
-    }
-
-    private void init() {
-        debugText = (TextView) findViewById(R.id.debug_text);
-        contentFrame = (FrameLayout) findViewById(R.id.content_frame);
-        contentFrame.addView(initializeForView(new MainGdx()));
-        slidingLayer = (SlidingLayer) findViewById(R.id.sliding_layer);
-        list = new BlocksExpendableList((ExpandableListView) findViewById(R.id.drawer_list), this);
     }
 
     @Subscribe
@@ -75,10 +77,10 @@ public class GDXActivity extends AndroidApplication {
                 break;
             case EDITOR_SCREEN:
                 list.populateList();
-                loadWorkspace(contentFrame.getRootView());
+                loadWorkspace(gdxFrame.getRootView());
                 break;
             case GAME_SCREEN:
-                GameScreen.loadGame(loadDataFromFile(contentFrame.getRootView()));
+                GameScreen.loadGame(loadDataFromFile(gdxFrame.getRootView()));
                 break;
         }
     }
@@ -86,18 +88,6 @@ public class GDXActivity extends AndroidApplication {
     @Subscribe
     public void onEvent(BlockButton.OpenLayer event) {
         slidingLayer.openLayer(true);
-    }
-
-    public void onDebugClick(View view) {
-        debugText.setText(Workspace.getDebugLog());
-    }
-
-    public void paintConnectors(View view) {
-        ConnectionArea.debug = !ConnectionArea.debug;
-    }
-
-    public void saveWorkspace(View view) throws IOException {
-        DataManager.saveBlocks(view.getContext().getFilesDir().getAbsolutePath(), saveName, Workspace.getWorkspaceItemsToSave());
     }
 
     public void loadWorkspace(View view) throws IOException, ClassNotFoundException {
@@ -108,15 +98,34 @@ public class GDXActivity extends AndroidApplication {
         return DataManager.loadBlocks(view.getContext().getFilesDir().getAbsolutePath(), saveName);
     }
 
-    public void deleteAll(View view) {
-        Workspace.clearWorkspace();
-    }
+    private void initDebugTools() {
+        SlidingLayer toolsLayer = (SlidingLayer) findViewById(R.id.tools_layer);
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View debugButtons = inflater.inflate(R.layout.toolset_debug, toolsLayer);
 
-    //todo refactoringplz
-    public void switchScreen(View v) {
-        if (MainGdx.currentScreen == ScreenEnum.GAME_SCREEN)
-            EventBus.getDefault().post(ScreenEnum.EDITOR_SCREEN);
-        else
-            EventBus.getDefault().post(ScreenEnum.GAME_SCREEN);
+        debugText = (TextView) findViewById(R.id.debug_text);
+        ((Button) debugButtons.findViewById(R.id.button_debug_log)).setOnClickListener(
+                v -> debugText.setText(Workspace.getDebugLog()));
+        ((Button) debugButtons.findViewById(R.id.button_debug_connectors)).setOnClickListener(
+                v -> ConnectionArea.debug = !ConnectionArea.debug);
+        ((Button) debugButtons.findViewById(R.id.button_debug_save)).setOnClickListener(
+                v -> DataManager.saveBlocks(v.getContext().getFilesDir().getAbsolutePath(), saveName, Workspace.getWorkspaceItemsToSave()));
+        ((Button) debugButtons.findViewById(R.id.button_debug_load)).setOnClickListener(
+                v -> {
+                    try {
+                        loadWorkspace(v);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+        ((Button) debugButtons.findViewById(R.id.button_debug_delete)).setOnClickListener(
+                v -> Workspace.clearWorkspace());
+        ((Button) debugButtons.findViewById(R.id.button_debug_test)).setOnClickListener(
+                v -> {
+                    if (MainGdx.currentScreen == ScreenEnum.GAME_SCREEN)
+                        EventBus.getDefault().post(ScreenEnum.EDITOR_SCREEN);
+                    else
+                        EventBus.getDefault().post(ScreenEnum.GAME_SCREEN);
+                });
     }
 }
