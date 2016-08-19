215891308c3fffa3119dc8497ea87b03ea7927eb
rilane <s10474@pjwstk.edu.pl>
2016-08-19 04:59:55 +0200

    refactoring

--- core/src/com/fancytank/gamegen/game/actor/TileType.java
+++ core/src/com/fancytank/gamegen/game/actor/TileType.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 
-import java.io.IOException;
 import java.io.Serializable;
 import java.util.HashMap;
 
